0a99d3815070a9543181936ee3f88ffbd870a26e
George J <georgejambooken@gmail.com>
2024-07-03 10:55:37 +0530

    added basic strategy

--- backtrader 1.py
+++ backtrader 1.py
@@ -1,7 +1,23 @@
 import backtrader as bt
+import datetime
+from strategy1 import TestStrategy
 
 cerebro = bt.Cerebro()
-cerebro.broker.set_cash(1000)
+cerebro.broker.set_cash(10000)
+
+data = bt.feeds.YahooFinanceCSVData(
+        dataname='oracle 1995_2014.csv',
+        # Do not pass values before this date
+        fromdate=datetime.datetime(2000, 1, 1),
+        # Do not pass values before this date
+        todate=datetime.datetime(2000, 12, 31),
+        # Do not pass values after this date
+        reverse=False)
+
+
+cerebro.adddata(data)
+cerebro.addstrategy(TestStrategy)
+
 print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
 cerebro.run()
 print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
