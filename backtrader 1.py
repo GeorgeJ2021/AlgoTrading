772d9c98cf06d828341a742e8428d6a83db5aa90
George J <georgejambooken@gmail.com>
2024-07-03 09:52:53 +0530

    started backtrader

---
+++ backtrader 1.py
@@ -0,0 +1,7 @@
+import backtrader as bt
+
+cerebro = bt.Cerebro()
+cerebro.broker.set_cash(1000)
+print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.run()
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
